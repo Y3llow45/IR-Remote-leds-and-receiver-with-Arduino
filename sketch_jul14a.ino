667fd746c8a00e04bd7ab12638af809bb26e2e84
Borky <68009977+Y3llow45@users.noreply.github.com>
2022-07-16 14:36:12 +0300

    Update sketch_jul14a.ino
    fixed some stuff

--- sketch_jul14a.ino
+++ sketch_jul14a.ino
@@ -16,8 +16,177 @@ void setup() {
   irrecv.enableIRIn();
 }
 
-void powerButtonRed() {
+void allOff(int de) {
+  digitalWrite(7, LOW);
+  delay(de);
+  digitalWrite(8, LOW);
+  delay(de);
+  digitalWrite(9, LOW);
+  delay(de);
+  digitalWrite(10, LOW);
+  delay(de);
+  digitalWrite(11, LOW);
+}
 
+void powerButtonRed() {
+  for (int i = 0; i <= 4; i++) {
+    if (bools[i] == true) {
+      for (int i = 7; i <= 11; i++) {
+        digitalWrite(i, LOW);
+        for (int i = 0; i <= 4; i++) {
+          bools[i] = false;
+        }
+      }
+      return;
+    } else {
+      for (int i = 7; i <= 11; i++) {
+        digitalWrite(i, HIGH);
+        for (int i = 0; i <= 4; i++) {
+          bools[i] = true;
+        }
+      }
+      return;
+    }
+  }
+}
+
+void firstLed() {
+  if (bools[0] == false) {
+    digitalWrite(LED1, HIGH);
+    bools[0] = true;
+  } else {
+    digitalWrite(LED1, LOW);
+    bools[0] = false;
+  }
+}
+
+void secondLed() {
+  if (bools[1] == false) {
+    digitalWrite(LED2, HIGH);
+    bools[1] = true;
+  } else {
+    digitalWrite(LED2, LOW);
+    bools[1] = false;
+  }
+}
+
+void thirdLed() {
+  if (bools[2] == false) {
+    digitalWrite(LED3, HIGH);
+    bools[2] = true;
+  } else {
+    digitalWrite(LED3, LOW);
+    bools[2] = false;
+  }
+}
+
+void fourthLed() {
+  if (bools[3] == false) {
+    digitalWrite(LED4, HIGH);
+    bools[3] = true;
+  } else {
+    digitalWrite(LED4, LOW);
+    bools[3] = false;
+  }
+}
+
+void fifthLed() {
+  if (bools[4] == false) {
+    digitalWrite(LED5, HIGH);
+    bools[4] = true;
+  } else {
+    digitalWrite(LED5, LOW);
+    bools[4] = false;
+  }
+}
+
+void bluePowerbutton() {
+  if (bools[0] == true) {
+    digitalWrite(LED1, LOW);
+    bools[0] = false;
+  } else {
+    digitalWrite(LED1, HIGH);
+    bools[0] = true;
+  } if (bools[1] == true) {
+    digitalWrite(LED2, LOW);
+    bools[1] = false;
+  } else {
+    digitalWrite(LED2, HIGH);
+    bools[1] = true;
+  } if (bools[2] == true) {
+    digitalWrite(LED3, LOW);
+    bools[2] = false;
+  } else {
+    digitalWrite(LED3, HIGH);
+    bools[2] = true;
+  } if (bools[3] == true) {
+    digitalWrite(LED4, LOW);
+    bools[3] = false;
+  } else {
+    digitalWrite(LED4, HIGH);
+    bools[3] = true;
+  } if (bools[4] == true) {
+    digitalWrite(LED5, LOW);
+    bools[4] = false;
+  } else {
+    digitalWrite(LED5, HIGH);
+    bools[4] = true;
+  }
+}
+
+void partyMode() {
+  for (int i = 7; i <= 11; i++) {
+    digitalWrite(i, LOW);
+    bools[0] == true;
+    bools[1] == false;
+    bools[2] == true;
+    bools[3] == false;
+    bools[4] == true;
+  }
+  for (int i = 7; i <= 11; i++) {
+    digitalWrite(i, HIGH);
+    delay(100);
+    digitalWrite(i, LOW);
+  }
+  for (int i = 11; i >= 7; i--) {
+    digitalWrite(i, HIGH);
+    delay(100);
+    digitalWrite(i, LOW);
+  }
+  for (int i = 7; i <= 11; i++) {
+    digitalWrite(i, HIGH);
+    delay(100);
+  }
+  allOff(100);
+  for (int i = 11; i >= 7; i--) {
+    digitalWrite(i, HIGH);
+    delay(100);
+  }
+  allOff(100);
+  for (int i = 1; i <= 2; i++) {
+    digitalWrite(7, HIGH);
+    delay(100);
+    digitalWrite(9, HIGH);
+    delay(100);
+    digitalWrite(11, HIGH);
+    delay(100);
+    for (int b = 1; b <= 2; b++) {
+      digitalWrite(7, LOW);
+      digitalWrite(9, LOW);
+      digitalWrite(11, LOW);
+      delay(100);
+      digitalWrite(8, HIGH);
+      digitalWrite(10, HIGH);
+      delay(100);
+      digitalWrite(7, HIGH);
+      digitalWrite(9, HIGH);
+      digitalWrite(11, HIGH);
+      delay(100);
+      digitalWrite(8, LOW);
+      digitalWrite(10, LOW);
+      delay(100);
+    }
+  }
 }
 
 void loop() {
@@ -26,95 +195,21 @@ void loop() {
     hexValue = String(results.value, HEX);
     Serial.println(hexValue);
     if (hexValue == "ff02fd") {
-      if (on == false) {
-        for (int i = 7; i <= 11; i++) {
-          digitalWrite(i, HIGH);
-          on = true;
-          for (int i = 0; i <= 4; i++) {
-            bools[i] = true;
-          }
-        }
-      } else {
-        for (int i = 7; i <= 11; i++) {
-          digitalWrite(i, LOW);
-          on = false;
-          for (int i = 0; i <= 4; i++) {
-            bools[i] = false;
-          }
-        }
-      }
+      powerButtonRed();
     } else if (hexValue == "ff807f") {
-      if (bools[0] == false) {
-        digitalWrite(LED1, HIGH);
-        bools[0] = true;
-      } else {
-        digitalWrite(LED1, LOW);
-        bools[0] = false;
-      }
+      firstLed();
     } else if (hexValue == "ff40bf") {
-      if (bools[1] == false) {
-        digitalWrite(LED2, HIGH);
-        bools[1] = true;
-      } else {
-        digitalWrite(LED2, LOW);
-        bools[1] = false;
-      }
+      secondLed();
     } else if (hexValue == "ffc03f") {
-      if (bools[2] == false) {
-        digitalWrite(LED3, HIGH);
-        bools[2] = true;
-      } else {
-        digitalWrite(LED3, LOW);
-        bools[2] = false;
-      }
+      thirdLed();
     } else if (hexValue == "ff20df") {
-      if (bools[3] == false) {
-        digitalWrite(LED4, HIGH);
-        bools[3] = true;
-      } else {
-        digitalWrite(LED4, LOW);
-        bools[3] = false;
-      }
+      fourthLed();
     } else if (hexValue == "ffa05f") {
-      if (bools[4] == false) {
-        digitalWrite(LED5, HIGH);
-        bools[4] = true;
-      } else {
-        digitalWrite(LED5, LOW);
-        bools[4] = false;
-      }
+      fifthLed();
     } else if (hexValue == "20df10ef") {
-      if (bools[0] == true) {
-        digitalWrite(LED1, LOW);
-        bools[0] == false;
-      } else {
-        digitalWrite(LED1, HIGH);
-        bools[0] == true;
-      } if (bools[1] == true) {
-        digitalWrite(LED2, LOW);
-        bools[1] == false;
-      } else {
-        digitalWrite(LED2, HIGH);
-        bools[1] == true;
-      } if (bools[2] == true) {
-        digitalWrite(LED3, LOW);
-        bools[2] == false;
-      } else {
-        digitalWrite(LED3, HIGH);
-        bools[2] == true;
-      } if (bools[3] == true) {
-        digitalWrite(LED4, LOW);
-        bools[3] == false;
-      } else {
-        digitalWrite(LED4, HIGH);
-        bools[3] == true;
-      } if (bools[4] == true) {
-        digitalWrite(LED5, LOW);
-        bools[4] == false;
-      } else {
-        digitalWrite(LED5, HIGH);
-        bools[4] == true;
-      }
+      bluePowerbutton();
+    } else if (hexValue == "ff926d") {
+      partyMode();
     }
     delay(50);
     irrecv.resume();
